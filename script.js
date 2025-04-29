b28d1cb8edb77fa026102440dc2d44adfcf76565
SilentStorm227 <stormpetal227@gmail.com>
2025-04-29 21:20:54 +0100

    tecnically done

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-//  document.addEventListener('keydown' , function(e){
-//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-// });
-
 document.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return; // If no audio element matches, exit the function
 
     audio.currentTime = 0; // Rewind to start if it's already playing
     audio.play(); // Play the audio
+    console.log(key)
 });
