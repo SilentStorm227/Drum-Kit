5df41a711ff8e9a81c9e0982dd6001755158e03e
SilentStorm227 <stormpetal227@gmail.com>
2025-04-29 20:25:49 +0100

    added music

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-window.addEventListener('keydown' , function(e){
-    console.log(e.key);
+//  document.addEventListener('keydown' , function(e){
+//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+// });
+
+document.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if (!audio) return; // If no audio element matches, exit the function
+
+    audio.currentTime = 0; // Rewind to start if it's already playing
+    audio.play(); // Play the audio
 });
